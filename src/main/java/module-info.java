ec4522c93b83c4b6317ef39f0916c7bfbf281cb3
Anthony Gough <abc@123.com.au>
2025-03-30 23:02:19 +1000

    Initial commit to respoitory for Week 6 Demo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.addressbook {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.addressbook to javafx.fxml;
+    exports com.example.addressbook;
+    exports com.example.addressbook.model;
+    opens com.example.addressbook.model to javafx.fxml;
+    exports com.example.addressbook.controller;
+    opens com.example.addressbook.controller to javafx.fxml;
+}
